65d1f480a916dabd606d8cfb2437e3b0644fb671
carlos ramirez <cgrs27@gmail.com>
2025-09-05 13:52:57 -0400

    configuracion para despliegue en produccion

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vhy!fna)_@#ez_dq#7nt0pewh-@*9zg8re$e2iw)j0ex_7=77tz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','','cgrs27.pythonanywhere.com.']
 
 
 # Application definition
@@ -128,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
